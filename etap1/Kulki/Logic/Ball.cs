74e982defe59ed5b6b653574c0a30cfaf61eb4fb
242541 <242541@edu.p.lodz.pl>
2023-03-26 17:36:11 +0200

    [Added] Etap 1

---
+++ etap1/Kulki/Logic/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Ball
+    {
+        private int x;
+        private int y;
+        private int radius;
+        private int velocity;
+
+        public Ball(int x, int y, int radius, int velocity)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Radius = radius;
+            this.Velocity = velocity;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Radius { get => radius; set => radius = value; }
+        public int Velocity { get => velocity; set => velocity = value; }
+    
+        public Vector2 getPosition()
+        {
+            return new Vector2(X, Y);
+        }
+    }
+}
---
+++ etap1/Kulki/Logic/BoundsKeeper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class BoundsKeeper
+    {
+        private int height;
+        private int width;
+
+        public BoundsKeeper(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height { get => height; set => height = value; }
+        public int Width { get => width; set => width = value; }
+
+        public bool xIsInBounds(int x, int radius) 
+        {
+            bool retValue = false;
+            int ctrl = 0;
+
+            if (x + radius <= width)
+            {
+                ctrl++;
+            }
+
+            if (x - radius >= 0)
+            {
+                ctrl++;
+            }
+
+            if(ctrl == 2)
+            {
+                retValue = true;
+            }
+
+            return retValue;
+        }
+
+        public bool yIsInBounds(int y, int radius) 
+        {
+            bool retValue = false;
+            int ctrl = 0;
+
+            if (y + radius <= height) 
+            {
+                ctrl++;
+            }
+
+            if (y - radius >= 0)
+            {
+                ctrl++;
+            }
+
+            if(ctrl == 2)
+            {
+                retValue = true;
+            }
+
+            return retValue;
+        }
+    }
+}
