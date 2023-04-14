2d6dd6d2196ebad3d799ec41a6a8ba538f636b5b
Paweł Stus <pawel.j.stus@gmail.com>
2023-04-14 22:21:34 +0200

    ViewModel Empty

---
+++ CrazyBallz/ViewModel/Commando.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class Commando : ICommand
+    {
+        private readonly Action method;
+        private readonly Func<bool>? ready;
+        public event EventHandler? CanExecuteChanged;
+
+        public Commando(Action method, Func<bool>? ready = null)
+        {
+            if (method is null) throw new ArgumentNullException("method");
+
+            this.method = method;
+            this.ready = ready;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if(ready is null)
+            {
+                return false;
+            }
+            else
+            {
+                return ready();
+            }
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
