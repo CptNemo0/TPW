ff575b1c37e8c1a3675dd67b0a8440438e7ec455
Paweł Stus <pawel.j.stus@gmail.com>
2023-04-14 16:16:22 +0200

    Empty BallManager and LogicApi

---
+++ CrazyBallz/Logic/LogicApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public abstract class LogicApi
+    {
+        public static LogicApi Instantiate(int boardHeight, int boardWidth)
+        {
+
+        }
+    }
+}
