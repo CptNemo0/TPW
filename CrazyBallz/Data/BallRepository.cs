f5497c955481beb52f70c09cd6dd166edeb8e01f
Paweł Stus <pawel.j.stus@gmail.com>
2023-04-14 16:06:51 +0200

    Added DataApi and BallRepository

---
+++ CrazyBallz/Data/BallRepository.cs
@@ -0,0 +1,36 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class BallRepository : DataApi
+    {
+        private List<IBall> balls = new();
+
+        public List<IBall> Balls { get => balls; set => balls = value; }
+
+        public override void AddBall(IBall ball)
+        {
+            this.balls.Add(ball);
+        }
+
+        public override int GetAmountOfBalls()
+        {
+            return this.balls.Count;
+        }
+
+        public override void RemoveAllBalls()
+        {
+            this.balls.Clear();
+        }
+
+        public override void RemoveBall(IBall ball)
+        {
+            this.balls.Remove(ball);
+        }
+    }
+}
