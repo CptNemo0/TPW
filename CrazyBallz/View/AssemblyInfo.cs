1fdf65a80430ab02c41714aa169133b3e3d1f72b
Paweł Stus <pawel.j.stus@gmail.com>
2023-04-15 15:16:32 +0200

    Obligatory Dune quite

--- CrazyBallz/View/AssemblyInfo.cs
+++ CrazyBallz/View/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 using System.Windows;
 
+//God created Arrakis to train the faithful.
+
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
