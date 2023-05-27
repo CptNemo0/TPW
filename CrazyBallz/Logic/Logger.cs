7521bb0e21eb3b1c981be2dfd49eb33df7e3d40b
Andrzej Szlaski <242545@edu.p.lodz.pl>
2023-05-27 17:56:05 +0200

    cleanup

--- CrazyBallz/Logic/Logger.cs
+++ CrazyBallz/Logic/Logger.cs
@@ -1,10 +1,5 @@
-﻿using Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using Newtonsoft.Json;
 using System.IO;
 
