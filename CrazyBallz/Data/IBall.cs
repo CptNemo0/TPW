368695cee6113fefe6c587c28d9846af2158a513
Andrzej Szlaski <242545@edu.p.lodz.pl>
2024-09-09 09:33:21 +0200

    fix of my doings

---
+++ CrazyBallz/Data/IBall.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+
+namespace Data
+{
+    public abstract class IBall
+    {
+        public abstract float Position_X { get; set; }
+        public abstract float Position_Y { get; set; }
+        public abstract int Radius { get; }
+        public abstract float Speed_X { get; set; }
+        public abstract float Speed_Y { get; set; }
+        public abstract int Mass { get; set; }
+        public abstract Vector2 BoardSize { get; set; }
+
+        public static IBall CreateBall(float postion_X, float postion_Y, int radius, float speed_X, float speed_Y, int mass)
+        {
+            return new Ball(postion_X, postion_Y, radius, speed_X, speed_Y, mass);
+        }
+    }
+}
